a65a33012279cc18eb87f480530f168849309d8b
FITAHIANA Nomeniavo joe <24nomeniavo@gmail.com>
2025-07-25 13:42:20 +0300

    and new widget named check_list

---
+++ app/frontend/lib/src/widgets/check_list.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+enum Status { Checked, Pedding }
+
+class CheckList extends StatelessWidget {
+  final String leftItem;
+  final String rightItem;
+  final Status status;
+
+  const CheckList({
+    super.key,
+    required this.leftItem,
+    required this.rightItem,
+    required this.status,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          child: Row(
+            spacing: 5,
+            children: [
+              Container(
+                child: Icon(Icons.check, color: Colors.white, size: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color:
+                      status == Status.Checked
+                          ? Theme.of(context).primaryColor
+                          : Colors.amber,
+                ),
+                padding: EdgeInsets.all(2.0),
+              ),
+              Text(leftItem, style: TextStyle(fontSize: 18)),
+            ],
+          ),
+        ),
+        Text(rightItem),
+      ],
+    );
+  }
+}
