3062d1dffc6efd2660e54f388278f38f96837a81
FITAHIANA Nomeniavo joe <24nomeniavo@gmail.com>
2025-08-05 19:40:15 +0300

    update config to use turso db

--- app/backend/app/core/config.py
+++ app/backend/app/core/config.py
@@ -14,14 +14,14 @@ JWT = jwt.Jwt(SECRET)
 TURSO_DB_URL = getenv("TURSO_DATABASE_URL")
 TURSO_DB_AUTH_TOKEN = getenv("TURSO_AUTH_TOKEN")
 
-DATABASE_URL = "sqlite+libsql:///database.db"
+DATABASE_URL = "sqlite+libsql:///replicated.db"
 
 ENGINE = create_engine(
     DATABASE_URL,
-    # connect_args={
-    #     "auth_token": TURSO_AUTH_TOKEN,
-    #     "sync_url": TURSO_DATABASE_URL,
-    # },
+    connect_args={
+        "auth_token": TURSO_DB_URL,
+        "sync_url": TURSO_DB_AUTH_TOKEN,
+    },
 )
 
 
