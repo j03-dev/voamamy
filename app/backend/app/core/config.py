e8544af98f789e22e537ec12c1b08a06029481eb
FITAHIANA Nomeniavo joe <24nomeniavo@gmail.com>
2025-08-05 20:05:05 +0300

    Fix: Configure database connection and ignore files
    Update the database configuration to use the correct environment
    variables. Also update the .gitignore to ignore all files ending in
    `.db*` instead of just `.db`.

--- app/backend/app/core/config.py
+++ app/backend/app/core/config.py
@@ -10,17 +10,14 @@ load_dotenv()
 SECRET = getenv("SECRET")
 JWT = jwt.Jwt(SECRET)
 
-
-TURSO_DB_URL = getenv("TURSO_DATABASE_URL")
-TURSO_DB_AUTH_TOKEN = getenv("TURSO_AUTH_TOKEN")
-
-DATABASE_URL = "sqlite+libsql:///replicated.db"
+TURSO_DATABASE_URL = getenv("TURSO_DATABASE_URL")
+TURSO_AUTH_TOKEN = getenv("TURSO_AUTH_TOKEN")
 
 ENGINE = create_engine(
-    DATABASE_URL,
+    "sqlite+libsql:///embedded.db",
     connect_args={
-        "auth_token": TURSO_DB_AUTH_TOKEN,
-        "sync_url": TURSO_DB_URL,
+        "auth_token": TURSO_AUTH_TOKEN,
+        "sync_url": TURSO_DATABASE_URL,
     },
 )
 
